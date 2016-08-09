0e3f99e9151ba37879c7083b71d5a0bf1c4fdc58
pamela rosales <>
2016-08-09 03:46:52 -0300

    Trabalhando em equipe com GitHub

---
+++ src/com/testes/github.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.testes;
+
+/**
+ *
+ * @author pamela
+ */
+public class github {
+        public static void main(String args[]){
+            System.out.println("Meu primeiro projeto com GitHub e Netbeans");
+        }
+}
